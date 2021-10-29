100f9d3c78b84464aec6bed4ec6e553cf97f1e9a
gasplant64 <gasplant64@snu.ac.kr>
2021-10-29 10:09:25 +0900

    data

--- simple_nn_v2/features/data_generator.py
+++ simple_nn_v2/features/data_generator.py
@@ -146,7 +146,7 @@ def load_structures(inputs, structure_file, structure_slicing, logfile,comm=None
                 file_path = compress_outcar(structure_file)
 
         if ase.__version__ >= '3.18.0':
-            structures = io.read(file_path, index=index, format=inputs['descriptor']['refdata_format'], parallel=False)
+            structures = io.read(file_path, index=index, format=inputs['descriptor']['refdata_format'], parallel=False if comm.rank == 1 else True)
         else:
             structures = io.read(file_path, index=index, format=inputs['descriptor']['refdata_format'], force_consistent=True, parallel=False)
     else:
